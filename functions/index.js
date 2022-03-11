569f770b3a6125af0b9de89356b90332e263afba
clairyitinggu <yitinggu@umass.edu>
2022-03-11 13:42:37 -0500

    combine two cloud function into index.js

--- functions/index.js
+++ functions/index.js
@@ -35,3 +35,27 @@ exports.createData = functions.firestore
   });
 //command line
 //firebase deploy
+exports.updateData = functions.firestore
+  .document("users/{docId}")
+  .onUpdate((change, context) => {
+    // Get current document value.
+    const newValue = change.after.data();
+    //const previousValue = change.before.data();
+    const name = newValue.name || "";
+    const invNumber = newValue.invNumber || "";
+    const public = newValue.invNumber.public || false;
+    // add current value to doi collection with same docId
+    db.collection("doi").doc(context.params.docId).set({
+      name: name,
+      invNumber: invNumber,
+      public: public,
+    });
+    //update the current value in users collection
+    db.collection("users").doc(context.params.docId).set({
+      name: name,
+      invNumber: invNumber,
+      public: public,
+    });
+
+    return context.params.docId;
+  });
